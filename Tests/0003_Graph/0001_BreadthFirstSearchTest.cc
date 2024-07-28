3abfdd7569fa8532f958b356ee37220d62a1b8fa
Debashis Nandi <debashisnandi576@gmail.com>
2024-07-28 18:15:33 +0530

    Update 0001_BreadthFirstSearchTest.cc

--- Tests/0003_Graph/0001_BreadthFirstSearchTest.cc
+++ Tests/0003_Graph/0001_BreadthFirstSearchTest.cc
@@ -5,7 +5,8 @@
 // Demonstrate some basic assertions.
 namespace BreadthFirstSearchTest
 {
-	TEST(BFSTesting, ShowBFSResultTest) {
+	TEST(BFSTesting, ShowBFSResultTest01) 
+    {
 		BFSGraph graph;
 
         graph.PushUndirectedEdge('s', 'r');
@@ -25,4 +26,17 @@ namespace BreadthFirstSearchTest
         string expectedResult = " r(1) s(0) t(2) u(3) v(2) w(1) x(2) y(3)";
         EXPECT_EQ(actualResult, expectedResult);
 	}
+
+    TEST(BFSTesting, ShowBFSResultTest02) 
+    {
+        BFSGraph graph;
+
+        graph.PushUndirectedEdge('s', 'r');
+
+        graph.BFS('s');
+
+        string actualResult = graph.ShowBFSResult();
+        string expectedResult = " r(1) s(0)";
+        EXPECT_EQ(actualResult, expectedResult);
+    }
 }
